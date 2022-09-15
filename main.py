b99f13ae181f52dcafa14304910dac2965a15805
Lockandkey <hopeful.genius@gmail.com>
2022-09-14 22:15:35 -0600

    added random name generation

--- main.py
+++ main.py
@@ -3,7 +3,43 @@ import pickle
 import random
 
 bands_path = f'{os.getcwd()}/bands'
-band_list = []
+
+
+def random_band_name():
+    name_table_alpha = [
+        'Silver _',
+        'Shining _',
+        '_ Free Company',
+        '_ Brigade',
+        'Blighted _',
+        'Spectral _',
+        'Quick _',
+        'Arcane _',
+        'Honored _',
+        'Banished _',
+        'Eternal _',
+        'Silent _'
+    ]
+
+    name_table_bravo = [
+        'Lion',
+        'Reveler',
+        'Ravager',
+        'Herald',
+        'Gryphon',
+        'Zephyr',
+        'Seeker',
+        'Slayer',
+        'Boot',
+        'Blade',
+        'Wand',
+        'Shield'
+    ]
+
+    name_table_plural = ['', 's']
+
+    band_name = random.choice(name_table_alpha).replace("_", random.choice(name_table_bravo)) + random.choice(name_table_plural)
+    return band_name
 
 
 def assign_new_random_hostility():
@@ -21,14 +57,18 @@ class AdventuringBand:
     band_name = ""
     hostility = 50
 
-    def __init__(self, name, hostile_percent=0):
-        self.band_name = name
+    def __init__(self, name=None, hostile_percent=None):
+        if name is None:
+            self.band_name = random_band_name()
+        else:
+            self.band_name = name
+
         self.hostility = hostile_percent
-        if self.hostility == 0:
+        if self.hostility is None:
             self.hostility = assign_new_random_hostility()
 
     def __str__(self):
-        return f'Band: {self.band_name}. Probability of conflict: {self.hostility}'
+        return f'Band: {self.band_name}. Probability of conflict: {self.hostility}%'
 
 
 def write_existing_bands(list_of_bands):
@@ -43,9 +83,10 @@ def read_existing_bands():
     return list_of_bands
 
 
-bands_list = [AdventuringBand("Saga"), AdventuringBand("Edda")]
+bands_list = [AdventuringBand(), AdventuringBand(), AdventuringBand(), AdventuringBand()]
 
 write_existing_bands(bands_list)
 read_list = read_existing_bands()
 
-print(f'Randomly selected {random.choice(read_list)}')
+for band in bands_list:
+    print(band)
